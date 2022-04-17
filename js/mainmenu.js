d56beb1b893fcee93775b2bb27215d604c572d67
Vasanthan Ravindran <vasanth.ravindran@gmail.com>
2022-04-17 12:30:31 -0400

    Rama Navami Pics

--- js/mainmenu.js
+++ js/mainmenu.js
@@ -13,11 +13,11 @@ class MainMenu extends HTMLElement {
                     <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Gallery</a>
                     <ul class="dropdown-menu">
                         <li class="nav-item"><a class="nav-link" href="ugadhi.html">Ugadi 2022</a></li>
-                        <li class="nav-item"><a class="nav-link" href="sivaratri.html">Sri Rama Navami</a></li>
+                        <li class="nav-item"><a class="nav-link" href="ramnavami.html">Sri Rama Navami</a></li>
                         <li class="nav-item"><a class="nav-link" href="day1.html">Sai Samasthan Day One</a></li>
                         <li class="nav-item"><a class="nav-link" href="sankranthi.html">Sankranthi</a></li>
                         <li class="nav-item"><a class="nav-link" href="sita-rama.html">Sri Sita Rama Kalyanam</a></li>
-                        <li class="nav-item"><a class="nav-link" href="ramnavami.html">Maha Sivaratri</a></li>
+                        <li class="nav-item"><a class="nav-link" href="sivaratri.html">Maha Sivaratri</a></li>
                     </ul>
                 </li>
                 <li class="nav-item"><a class="nav-link" href="index.html#contact">Contact</a></li>
