9149cf4309be6958fa869d89ebde230c2494c4f0
Vasanthan Ravindran <vasanth.ravindran@gmail.com>
2022-04-17 11:09:14 -0400

    changes

--- js/mainmenu.js
+++ js/mainmenu.js
@@ -12,14 +12,14 @@ class MainMenu extends HTMLElement {
                 <li class="nav-item submenu dropdown">
                     <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Gallery</a>
                     <ul class="dropdown-menu">
-                        <li class="nav-item"><a class="nav-link" href="ugadhi.html">Ugadi 2019</a></li>
+                        <li class="nav-item"><a class="nav-link" href="ugadhi.html">Ugadi 2022</a></li>
                         <li class="nav-item"><a class="nav-link" href="day1.html">Sai Samasthan Day One</a></li>
                         <li class="nav-item"><a class="nav-link" href="sankranthi.html">Sankranthi</a></li>
                         <li class="nav-item"><a class="nav-link" href="sita-rama.html">Sri Sita Rama Kalyanam</a></li>
                         <li class="nav-item"><a class="nav-link" href="sivaratri.html">Maha Sivaratri</a></li>
                     </ul>
                 </li>
-                <li class="nav-item"><a class="nav-link" href="#contact">Contact</a></li>
+                <li class="nav-item"><a class="nav-link" href="index.html#contact">Contact</a></li>
             </ul>
         </div>
         `
