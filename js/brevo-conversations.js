641bf4ac9a81b6e9f13d88dfeaf21f7f4c1d6466
Vasanthan Ravindran <vasanth.ravindran@gmail.com>
2024-10-14 19:04:45 -0400

    Reinitiating the site

---
+++ js/brevo-conversations.js
@@ -0,0 +1,10 @@
+(function(d, w, c) {
+    w.BrevoConversationsID = '62378522ac3d595c54488e52';
+    w[c] = w[c] || function() {
+        (w[c].q = w[c].q || []).push(arguments);
+    };
+    var s = d.createElement('script');
+    s.async = true;
+    s.src = 'https://conversations-widget.brevo.com/brevo-conversations.js';
+    if (d.head) d.head.appendChild(s);
+})(document, window, 'BrevoConversations');
